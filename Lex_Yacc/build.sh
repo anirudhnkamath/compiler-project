0e807c5406509f0c7fa9771dd097bd54b71f562b
anirudhnkamath <anirudhnkamath@gmail.com>
2025-11-06 23:32:50 +0530

    added submission files

--- Lex_Yacc/build.sh
+++ Lex_Yacc/build.sh
@@ -3,7 +3,7 @@ set -e
 bison -dy yacc.y
 lex lex.l
 gcc lex.yy.c y.tab.c ./../Parse_Tree/parse_tree.c ./../Symbol_Table/global_symbol_table.c ./../Type_Table/type_table.c ./../Three_addr_code_gen/Tac_gen.c -o final
-./final ./../Sample_input/input_1.txt
+./final ./../Sample_input/input_2.txt
 cp targetFile.txt ./optimize/targetFile.txt
 
 cd optimize
